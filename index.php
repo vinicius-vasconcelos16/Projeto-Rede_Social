a19b907f99d18bc98624fb7bd46c5daff2c8f642
Luis Vinicius Vasconcelos Teixeira <luisviniciusvasconcelost@gmail.com>
2023-05-07 18:11:45 -0300

    Main Config

--- index.php
+++ index.php
@@ -49,8 +49,127 @@
    </ul>
   </div>  
  </header>
- <main>
-    <h1>Menu</h1>
+ <main style="padding: 0px;">
+    <div class="container text-center" style="margin: 0;">
+        <div class="row align-items-start">
+          <div class="col-sm-3 fixed" style="background-color: lightgrey; margin: 40px;margin-right: 4vw; padding: 0px; border: 2px solid black;">
+              <img src="img/sem-foto.png" class="img-fluid" alt="unloged-user.png">
+          </div>
+          <div class="col-sm-8 p-3" style="border-left: 2px solid black;">
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="paisagem">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+            <div class="row g-0 bg-body-secondary position-relative" style="border-radius: 20px; border: 2px solid #F45BD9; margin-top: 40px; margin-left: 10px; margin-bottom: 20px;">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/paisagem1.avif" style="border: 2px solid black; border-radius: 20px;" class="w-100" alt="...">
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0">Columns with stretched link</h5>
+                <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                <a href="#" class="stretched-link">Go somewhere</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+    </div>
  </main>
 
 </body>
